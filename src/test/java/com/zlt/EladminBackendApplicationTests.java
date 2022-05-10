30f9e8554fcff26e08f017818c5a1fdbe5ead5fc
zlt <3495626609@qq.com>
2022-05-10 17:01:28 +0800

    1.初始化项目

---
+++ src/test/java/com/zlt/EladminBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zlt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EladminBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
